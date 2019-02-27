40c1f4042ed00e8766907fa63797b96506a90e07
Juan Manuel Serrano <juanmanuel.serrano@hablapps.com>
2019-02-27 21:31:14 +0100

    TParser

--- src/test/scala/typefun/TypedSprintf.scala
+++ src/test/scala/typefun/TypedSprintf.scala
@@ -8,7 +8,7 @@ import scala.util.Try
 // SECTION 4.1 TYPED SPRINTF
 
 trait Read[T]{
-  def read(s: String): Option[(T, String)]
+  def read(str: String): Option[(T, String)]
 }
 
 object Read{
@@ -16,8 +16,24 @@ object Read{
   def apply[A](implicit R: Read[A]): Read[A] = R
 
   implicit val IntRead = new Read[Int]{
-    def read(s: String): Option[(Int, String)] =
-      Try(Integer.parseInt(s)).toOption map ((_, ""))
+    val IntPrefix = """(\d+)(.*)""".r
+
+    def read(str: String): Option[(Int, String)] = str match {
+      case IntPrefix(i, tail) => Some((Integer.parseInt(i), tail))
+      case _ => None
+    }
+  }
+
+  implicit val StrRead = new Read[String]{
+    def StrPrefix(prefix: String) = """${prefix}(.*)""".r
+
+    def read(str: String): Option[(String, String)] = {
+      val R = StrPrefix(str)
+      str match {
+        case R(tail) => Some((str, tail))
+        case _ => None
+      }
+    }
   }
 }
 
@@ -50,49 +66,32 @@ class FmtSpec extends FunSpec with Matchers{
 }
 
 object NaiveApproach{
-  trait SPrintf[F <: Fmt]{
+  trait TPrinter[F <: Fmt]{
     type Args
 
     def apply(f: F): Args => String
   }
 
-  object SPrintf{
+  object TPrinter{
 
-    implicit val LitSPrintf = new SPrintf[Lit]{
+    implicit val LitTPrinter = new TPrinter[Lit]{
       type Args = Unit
 
       def apply(f: Lit): Unit => String =
         _ => f.s
     }
 
-    implicit def ValSPrintf[A: Read: Show] = new SPrintf[Val[A]]{
+    implicit def ValTPrinter[A: Read: Show] = new TPrinter[Val[A]]{
       type Args = A
 
       def apply(f: Val[A]): A => String =
         Show[A].write
     }
 
-    // trait ConcF1[F1, F2]{
-    //   type Out
-    // }
-
-    // object ConcF1{
-
-    //   def StringConc[F] = new ConcF1[String, F]{
-    //     type Out = F
-    //   }
-
-    //   def F1Conc[A, F1, F2](implicit S: ConcF1[F1, F2]) = new ConcF1[A => F1, F2]{
-    //     type Out = A => S.Out
-
-    //     def split()
-    //   }
-    // }
-
-    implicit def CmpSPrintf[F1 <: Fmt, A1, F2 <: Fmt, A2](implicit
-        P1: SPrintf[F1]{ type Args = A1 },
-        P2: SPrintf[F2]{ type Args = A2 }) =
-      new SPrintf[Cmp[F1, F2]]{
+    implicit def CmpTPrinter[F1 <: Fmt, A1, F2 <: Fmt, A2](implicit
+        P1: TPrinter[F1]{ type Args = A1 },
+        P2: TPrinter[F2]{ type Args = A2 }) =
+      new TPrinter[Cmp[F1, F2]]{
         type Args = (A1, A2)
 
         def apply(f: Cmp[F1, F2]): Args => String = {
@@ -102,7 +101,7 @@ object NaiveApproach{
 
     object Syntax{
 
-      implicit class FmtOps[F <: Fmt, A](f: F)(implicit P: SPrintf[F]{ type Args = A }){
+      implicit class FmtOps[F <: Fmt, A](f: F)(implicit P: TPrinter[F]{ type Args = A }){
         def apply(args: A): String =
           P(f)(args)
       }
@@ -110,51 +109,127 @@ object NaiveApproach{
   }
 }
 
-trait SPrintf[F <: Fmt, X]{
-  type Out
-}
-
-object SPrintf{
-
-  implicit val LitSPrintf = new SPrintf[Lit, X]{
-    type Out = X
-
-    def apply(fmt: Lit): (String => X) => X =
-      cont => cont(fmt.s)
-
-  }
-
-  implicit def ValSPrintf[A: Read: Show] = new SPrintf[Val[A], X]{
-    type Out = A => X
-
-    def apply(fmt: Val[A]): ((A => String) => X) => X =
-      cont => cont(fmt.Show[A].write)
-  }
-
-  implicit def CmpSPrintf[F1 <: Fmt, O1, F2 <: Fmt, O2, X](implicit
-      P2: SPrintf[F2, X]{ type Out = O2 },
-      P1: SPrintf[F1, O2]{ type Out = O1 }) =
-    new SPrintf[Cmp[F1, F2], X]{
-      type Out = O1
-
-      def apply(fmt: Cmp[F1, F2]): (O1 => X) => X =
-
-    }
-
-  object Syntax{
-
-    implicit class FmtOps[F <: Fmt, A](f: F)(implicit P: SPrintf[F]{ type Out = A }){
-      val print: A =
-        P(f)
-    }
-  }
-}
-
-class SPrintfSpec extends FunSpec with Matchers{
-  import SPrintf.Syntax._
+class TPrinterNaiveSpec extends FunSpec with Matchers{
+  import NaiveApproach._
+  import NaiveApproach.TPrinter.Syntax._
 
   Lit("hola")(()) shouldBe "hola"
   Val[Int].apply(1) shouldBe "1"
   Cmp(Lit("hola, "), Cmp(Lit("number "), Val[Int])).apply(((), ((), 1))) shouldBe
     "hola, number 1"
 }
+
+trait TPrinter[F <: Fmt, X]{
+  type Out
+
+  def apply(fmt: F)(cont: String => X): Out
+}
+
+object TPrinter{
+
+  type Aux[F <: Fmt, X, _Out] = TPrinter[F, X]{ type Out = _Out }
+
+  implicit def LitTPrinter[X] = new TPrinter[Lit, X]{
+    type Out = X
+
+    def apply(fmt: Lit)(cont: String => X): X =
+      cont(fmt.s)
+  }
+
+  implicit def ValTPrinter[A: Read: Show, X] = new TPrinter[Val[A], X]{
+    type Out = A => X
+
+    def apply(fmt: Val[A])(cont: String => X): A => X =
+      cont compose Show[A].write
+  }
+
+  implicit def CmpTPrinter[F1 <: Fmt, O1, F2 <: Fmt, O2, X](implicit
+      P2: TPrinter[F2, X]{ type Out = O2 },
+      P1: TPrinter[F1, O2]{ type Out = O1 }) =
+    new TPrinter[Cmp[F1, F2], X]{
+      type Out = O1
+
+      def apply(fmt: Cmp[F1, F2])(cont: String => X): O1 =
+        P1(fmt.f1){ s1 =>
+          P2(fmt.f2){ s2 =>
+            cont(s1 ++ s2): X
+          }: O2
+        }: O1
+
+    }
+
+  object Syntax{
+
+    def sprintf[F <: Fmt, Out](f: F)(implicit
+        P: TPrinter.Aux[F, String, Out]): Out =
+      P(f)(identity)
+
+    // implicit class FmtOps[F <: Fmt, _Out](f: F)(implicit
+    //     P: TPrinter[F, String]{ type Out = _Out }){
+    //   val printf: _Out =
+    //     P(f)(identity)
+    // }
+  }
+}
+
+class TPrinterSpec extends FunSpec with Matchers{
+  import TPrinter.Syntax._
+
+  sprintf(Lit("hola")) shouldBe "hola"
+  sprintf(Val[Int]).apply(1) shouldBe "1"
+  sprintf(Cmp(Lit("hola, "), Cmp(Lit("number "), Val[Int]))).apply(1) shouldBe
+    "hola, number 1"
+}
+
+trait TParser[F <: Fmt, X]{
+  type Out
+
+  def apply(f: F): String => Option[(Out, X)]
+}
+
+object TParser{
+
+  type Aux[F <: Fmt, X, _Out] = TParser[F, X]{ type Out = _Out }
+
+  def LitParser[X] = new TParser[Lit, X]{
+    type Out = Unit
+
+    def apply(f: Lit)(cont: String => X): String => Option[(Unit, X)] =
+      Read[String].read(_).map{
+        case (_, tail) => ((), cont(tail))
+      }
+  }
+
+  def ValParser[T: Read, X] = new TParser[Val[T], X]{
+    type Out = T
+
+    def apply(f: Val[T])(cont: String => X): String => Option[(T, X)] =
+      Read[T].read(_).map{
+        case (t, tail) => (t, cont(tail))
+      }
+  }
+
+  def CmpParser[F1 <: Fmt, O1, F2 <: Fmt, O2, X](implicit
+      P1: TParser.Aux[F1, O2, O1],
+      P2: TParser.Aux[F2, X, O2]) = new TParser[Cmp[F1, F2], X]{
+    type Out = O1
+
+    def apply(f: Cmp[F1, F2])(cont: String => X): String => Option[(O1, X)] =
+      ???
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
